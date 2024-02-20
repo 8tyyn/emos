c4234ab8c1aa32fd793de8fa7e57e8d23cef3964
haythem mardassi <haythemmardassi17@gmail.com>
2024-02-20 18:39:09 +0100

    added login admin page

---
+++ loginAdmin.php
@@ -0,0 +1,48 @@
+<?php
+session_start();
+$servername = "localhost";
+$serveruser = "root";
+$password = "";
+$dsn = "mysql:host=$servername;dbname=emos";
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    http_response_code(200);
+}
+try {
+    $BDD = new PDO($dsn, $serveruser, $password);
+    echo "connexion reussi";
+} catch (PDOException $e) {
+    echo "erreur" . $e->getMessage();
+}
+echo $_POST['email'];
+   $email = $_POST['email'];
+   $password = $_POST['password'];
+   if(!isset($_POST['password']) || !isset($_POST['email']) ) {
+        echo "Invalid"; 
+        exit();
+    }
+    try {
+        $query = "SELECT * FROM administrateur WHERE email=:email AND password=:password";
+    $stmnt = $BDD->prepare($query);
+    $stmnt->bindParam(':email',$email);
+    $stmnt->bindParam(':password',$password);
+    $stmnt->execute();
+    $rowCount = $stmnt->rowCount();
+    $res = $stmnt->fetch(PDO::FETCH_ASSOC);
+    }
+    catch (Exception $e) {
+        echo $e; 
+    }
+    if($rowCount !=0) {
+        print_r($res); 
+        $_SESSION['email']= $email ;
+        $_SESSION['password']=$password; 
+        header('location: index.php'); 
+        exit(); 
+    }
+    else {
+        header('location: loginAdmin.html'); 
+}   
+
+
+
+?>
